ccc3ea6c4b26fe9637e48e3cd77aa9c428adba90
sumanthz <71588615+sumanthz@users.noreply.github.com>
2023-02-19 11:15:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Card from "./components/Card"
+import data from "./data"
+
+
+
+
+export default function App() {
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                {...item}
+                
+            />
+        )
+    })        
+    
+            // <Hero />
+    return (
+        <div>
+            <Navbar />
+            <section className="cards-list">
+                {cards}
+            </section>
+        </div>
+    )
+}
---
+++ data.js
@@ -0,0 +1,106 @@
+export default [
+    {
+        id: 1,
+        title: "Faulkner Ave",
+     
+        price: 4500,
+        coverImg: "house1.jpg",
+        stats: {
+            rating: 5.0,
+            Bathrooms: 3,
+            Beds:2,
+            Area:"9x8"
+        },
+        location: "909 Woodland St,Michigan",
+        openSpots: 0,
+    },
+    {
+        id: 2,
+        title: "Crystal",
+    
+        price: 125,
+        coverImg: "house2.jpeg",
+       stats: {
+            rating: 5.0,
+            Bathrooms: 3,
+            Beds:2,
+            Area:"9x8"
+        },
+        location: "Online",
+        openSpots: 27,
+    },
+    {
+        id: 3,
+        title: "CoveSteel",
+      
+        price: 50,
+        coverImg: "house3.jpeg",
+         stats: {
+            rating: 5.0,
+            Bathrooms: 3,
+            Beds:2,
+            Area:"9x8"
+        },
+        location: "Norway",
+        openSpots: 3,
+    },
+     {
+        id: 4,
+        title: "Palm Harbour",
+       
+        price: 50,
+        coverImg: "house4.jpeg",
+        stats: {
+            rating: 5.0,
+            Bathrooms: 3,
+            Beds:4,
+            Area:"9x8"
+        },
+        location: "Norway",
+        openSpots: 3,
+    },
+      {
+        id: 7,
+        title: "west side",
+      
+        price: 50,
+        coverImg: "house6.jpeg",
+         stats: {
+            rating: 5.0,
+            Bathrooms: 3,
+            Beds:4,
+            Area:"9x8"
+        },
+        location: "Norway",
+        openSpots: 3,
+    }, {
+        id: 8,
+        title: "Ring top",
+      
+        price: 50,
+        coverImg: "house6.jpeg",
+         stats: {
+            rating: 5.0,
+            Bathrooms: 4,
+            Beds:2,
+            Area:"9x8"
+        },
+        location: "Norway",
+        openSpots: 3,
+    },
+     {
+        id: 9,
+        title: "Mustach",
+       
+        price: 50,
+        coverImg: "house6.jpeg",
+        stats: {
+            rating: 5.0,
+            Bathrooms: 2,
+            Beds:2,
+            Area:"9x8"
+        },
+        location: "Norway",
+        openSpots: 3,
+    }
+]
